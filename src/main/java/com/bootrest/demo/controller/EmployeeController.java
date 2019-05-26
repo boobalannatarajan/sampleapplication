9c9f3cd05c15dab53deb208ab5d391b4970ced7a
boobalan <boobalan.natarajan@cognizant.com>
2019-05-26 13:02:23 +0530

    initial version of spring boot rest application

---
+++ src/main/java/com/bootrest/demo/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.bootrest.demo.controller;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bootrest.demo.model.Employee;
+
+@RestController
+public class EmployeeController {
+	
+	@RequestMapping("/")
+    public List<Employee> getEmployees() 
+    {
+		List<Employee> employeesList = new ArrayList<Employee>();
+		employeesList.add(new Employee(716631,"boobalan","natarajan","boobalan.natarajan@cognizant.com"));
+		return employeesList;
+    }
+
+}
+

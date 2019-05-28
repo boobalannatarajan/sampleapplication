243876081dd2b2d029de3324d4af422213b8d2dc
boobalan <boobalan.natarajan@cognizant.com>
2019-05-28 22:07:34 +0530

    POM file change

--- src/main/java/com/bootrest/demo/controller/EmployeeController.java
+++ src/main/java/com/bootrest/demo/controller/EmployeeController.java
@@ -11,12 +11,17 @@ import com.bootrest.demo.model.Employee;
 public class EmployeeController {
 	
 	@RequestMapping("/")
+    public String getWelcomeNote() 
+    {
+		return "Welcome to CDE ML community practise session";
+    }
+	@RequestMapping("/empDetails")
     public List<Employee> getEmployees() 
     {
 		List<Employee> employeesList = new ArrayList<Employee>();
 		employeesList.add(new Employee(716631,"boobalan","natarajan","boobalan.natarajan@cognizant.com"));
+		employeesList.add(new Employee(916000,"siva","balan","siva.balan@cognizant.com"));
 		return employeesList;
     }
-
 }
 
